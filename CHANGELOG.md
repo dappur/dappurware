3c8190cc58c9dcf64614026fc16caa8b66122181
Jeff Thomas <jeffmthomas@gmail.com>
2018-04-02 11:36:02 -0700

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## [1.0.0] - 2018-04-02
+### Added
+- Separated Dappurware from the framework.
+
+
+[Unreleased]: https://github.com/dappur/dappurware/compare/v1.0.0...HEAD
+[1.0.0]: https://github.com/dappur/dappurware/v1.0.0
---
+++ app/src/Dappurware/Dappurware.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Dappur\Dappurware;
+
+use Interop\Container\ContainerInterface;
+
+class Dappurware
+{
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function __get($property)
+    {
+        return $this->container->get($property);
+    }
+}
